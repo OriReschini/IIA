6f6dac7e89b4caa53f77df2748f966b032f5c6ca
OriReschini <orireschini@gmail.com>
2019-03-30 19:42:37 -0300

    Ejercicio 3

--- Prolog/familia.pl
+++ Prolog/familia.pl
@@ -4,6 +4,7 @@ padre(luis,jorge).
 padre(luis,ines).
 padre(jorge,diego).
 
+% 3.1
 % a) ¿Quién es el padre de Inés?
 % padre(Quien, ines).
 
@@ -22,6 +23,8 @@ padre(jorge,diego).
 % f) Consultar si Juan es padre.
 % padre(juan,_).
 
+
+% 3.2
 abuelo_paterno(X, Y) :- padre(X, Z), padre(Z, Y).
 
 % a) ¿Quién es el abuelo de Diego?
@@ -33,11 +36,35 @@ abuelo_paterno(X, Y) :- padre(X, Z), padre(Z, Y).
 madre(ana,luis).
 madre(lia,maria).
 madre(lia,rosa).
+% agrego otros
+madre(ana, lia).
 
+% 3.3
 abuelo_materno(X, Y) :- padre(X, Z), madre(Z, Y).
 
+
+% 3.4
 abuelo(X, Y) :- abuelo_paterno(X, Y).
 abuelo(X, Y) :- abuelo_materno(X, Y).
 
 % Consultar por todos los pares de la relación abuelo
 % abuelo(X, Y).
+
+
+% 3.5
+%  Definir la relación hermano y obtener todos los pares de la misma
+hermano(X, Y) :- madre(Z, X), madre(Z, Y), padre(W, X), padre(W, Y), X\==Y.
+% hermano(X, Y).
+
+
+% 3.6
+% Definir la relación tio
+tio(X, Y) :- (hermano(X, Z), madre(Z, Y)); (hermano(X, Z), padre(Z, Y)).
+
+
+% 3.7
+% Defina la relación ancestro como clausura transitiva de las relaciones madre y
+% padre según la siguiente definición: “Una persona X es el ancestro de una
+% persona Y si X es el padre o madre de Y, o si existe una persona Z tal que Z es
+% padre o madre de Y y además X es ancestro de ese mismo Z
+ancestro(X, Y) :- padre(X, Y); madre(X, Y); (padre(Z, Y); madre(Z, Y)), ancestro(X, Z).
